6f58d131da9d32f2e98746d067620cd9825c7b17
Gabriel <gabrielfunes100202@gmail.com>
2024-05-09 17:49:35 -0300

    Añadiendo color primario a taliwnd

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import { About } from "./pages/About";
+import { Header } from "./components/Global/Header";
 
 export default function App() {
   return (
     <Router>
-      <h1>Hola</h1>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
