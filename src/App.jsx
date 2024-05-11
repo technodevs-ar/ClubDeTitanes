1e1b0e4647810762424170824eb08b9a0987f206
Gabriel <gabrielfunes100202@gmail.com>
2024-05-11 19:50:24 -0300

    Agrego parallax a espacio titanes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import { About } from "./pages/About";
 import { Header } from "./components/Global/Header";
+import { ParallaxProvider } from "react-scroll-parallax"
 
 export default function App() {
   return (
     <Router>
+      <ParallaxProvider>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
+      </ParallaxProvider>
     </Router>
   )
 }
