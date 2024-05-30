c8c431e1b92e095c5c75a874da2d8c65bd0f206d
fabricastro <fabricastrook@gmail.com>
2024-05-29 22:46:36 -0300

    agrego link wsp

--- src/components/Home/SectionContact.jsx
+++ src/components/Home/SectionContact.jsx
@@ -3,7 +3,7 @@ import './contact.css'
 export const SectionContact = () => {
   return (
     <section className="bg-alternative-950/80 flex flex-row py-32 justify-center items-center px-8 relative" id='contacto'>
-      <div className="container flex px-8"> 
+      <div className="container flex px-8">
         <div className="flex flex-col items-center container text-center">
           <h1 className="bg-gradient-to-b espacio-titanes from-primary-200 via-primary-400 to-primary-600 inline-block text-center text-transparent bg-clip-text text-3xl sm:text-4xl md:text-4xl font-bold mb-7">
             ¡Empezá tu transformación!
@@ -16,12 +16,14 @@ export const SectionContact = () => {
             las herramientas y el apoyo necesarios para que puedas alcanzar tus metas.
           </p>
           <div className="flex gap-5 mt-12 flex-col lg:flex-row">
-            <button className="py-5 px-8 bg-gradient-to-r from-primary-700 to-primary-500 rounded-md text-base sm:text-lg text-gray-200 font-bold w-56 sm:w-96 h-20 sm:h-16">
+            {/* <button className="py-5 px-8 bg-gradient-to-r from-primary-700 to-primary-500 rounded-md text-base sm:text-lg text-gray-200 font-bold w-56 sm:w-96 h-20 sm:h-16">
               Contáctate por mail
-            </button>
-            <button className="py-5 px-8 bg-gradient-to-r from-green-700 to-green-500 rounded-md text-base sm:text-lg text-gray-200 font-bold w-56 sm:w-96 h-20 sm:h-16 mt-7">
-              Enviar Whatsapp
-            </button>
+            </button> */}
+            <a href="https://wa.me/+5491126351121" target="_blank" rel="noopener noreferrer">
+              <button className="py-5 px-8 bg-gradient-to-r from-green-700 to-green-500 rounded-md text-base sm:text-lg text-gray-200 font-bold w-56 sm:w-96 h-20 sm:h-16 mt-7">
+                Enviar Whatsapp
+              </button>
+            </a>
           </div>
         </div>
       </div>
